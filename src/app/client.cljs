4a18b704b7a5802d8f2343ab4a43f4355efa0825
Tony Kay <tony.kay@gmail.com>
2019-09-04 15:04:28 -0700

    merged ui to full stack ex

--- src/app/client.cljs
+++ src/app/client.cljs
@@ -2,12 +2,11 @@
   (:require
     [com.fulcrologic.fulcro.application :as app]
     [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
-    [com.fulcrologic.fulcro.dom :as dom]
+    [com.fulcrologic.fulcro.dom :as dom :refer [div ul li h3 label]]
     [com.fulcrologic.fulcro.networking.http-remote :as http]
     [com.fulcrologic.fulcro.algorithms.merge :as merge]
     [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
     [com.fulcrologic.fulcro.algorithms.data-targeting :as targeting]))
-
 
 (defsc Car [this {:car/keys [id model] :as props}]
   {:query         [:car/id :car/model]
@@ -19,10 +18,6 @@
 
 (def ui-car (comp/factory Car {:keyfn :car/id}))
 
-(defmutation make-older [{:person/keys [id]}]
-  (action [{:keys [state]}]
-    (swap! state update-in [:person/id id :person/age] inc)))
-
 (defsc Person [this {:person/keys [id name age cars] :as props}]
   {:query         [:person/id :person/name :person/age {:person/cars (comp/get-query Car)}]
    :ident         :person/id
@@ -32,37 +27,57 @@
                    :person/cars [{:id 40 :model "Leaf"}
                                  {:id 41 :model "Escort"}
                                  {:id 42 :model "Sienna"}]}}
-  (dom/div
-    (dom/div "Name: " name)
-    (dom/div "Age: " age)
-    (dom/button {:onClick #(comp/transact! this [(make-older {:person/id id})])} "Make older")
-    (dom/h3 "Cars")
-    (dom/ul
-      (map ui-car cars))))
+  (let [onClick (comp/get-state this :onClick)]
+    (div :.ui.segment
+      (div :.ui.form
+        (div :.field
+          (label {:onClick onClick} "Name: ")
+          name)
+        (div :.field
+          (label "Age: ")
+          age)
+        (dom/button :.ui.button {:onClick (fn []
+                                            (comp/transact! this
+                                              `[(make-older ~{:person/id id})]
+                                              {:refresh [:person-list/people]}))}
+          "Make Older")
+        (h3 {} "Cars")
+        (ul {}
+          (map ui-car cars))))))
 
 (def ui-person (comp/factory Person {:keyfn :person/id}))
 
-(defsc Sample [this {:root/keys [person]}]
-  {:query         [{:root/person (comp/get-query Person)}]
-   :initial-state {:root/person {:id 1 :name "Bob"}}}
-  (dom/div
-    (when person
-      (ui-person person))))
+(defsc PersonList [this {:person-list/keys [people]}]
+  {:query         [{:person-list/people (comp/get-query Person)}]
+   :ident         (fn [] [:component/id ::person-list])
+   :initial-state {:person-list/people [{:id 1 :name "Bob"}
+                                        {:id 2 :name "Sally"}]}}
+  (let [cnt (reduce
+              (fn [c {:person/keys [age]}]
+                (if (> age 30)
+                  (inc c)
+                  c))
+              0
+              people)]
+    (div :.ui.segment
+      (h3 :.ui.header "People")
+      (div "Over 30: " cnt)
+      (dom/ul
+        (map ui-person people)))))
 
-(defonce APP (app/fulcro-app {:remotes
-                              {:remote (http/fulcro-http-remote {})}}))
+(def ui-person-list (comp/factory PersonList))
+
+(defsc Root [this {:root/keys [list]}]
+  {:query         [{:root/list (comp/get-query PersonList)}]
+   :initial-state {:root/list {}}}
+  (dom/div
+    (dom/h3 "Application")
+    (ui-person-list list)))
+
+(defonce APP (app/fulcro-app {:remotes {:remote (http/fulcro-http-remote {})}}))
 
 (defn ^:export init []
-  (app/mount! APP Sample "app"))
+  (app/mount! APP Root "app"))
 
 (comment
-
-  (comp/transact! APP [(make-older {:person/id 1})])
-
-  (app/current-state APP)
-
-  (merge/merge-component! APP Person {:person/id 1 :person/age 20})
-
-  (comp/get-initial-state Sample)
-
   )
