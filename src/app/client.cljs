ae997c40de8e859b1f3755e1312f8d489e174679
Tony Kay <tony.kay@gmail.com>
2019-09-04 14:00:23 -0700

    rendering workings

--- src/app/client.cljs
+++ src/app/client.cljs
@@ -4,17 +4,17 @@
     [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
     [com.fulcrologic.fulcro.dom :as dom :refer [div ul li h3 label]]
     [com.fulcrologic.fulcro.algorithms.merge :as merge]
+    [com.fulcrologic.fulcro.rendering.keyframe-render :as keyframe]
     [com.fulcrologic.fulcro.algorithms.denormalize :as fdn]
     [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
     [com.fulcrologic.fulcro.algorithms.data-targeting :as targeting]))
-
-(def ui-number-format (interop/react-factory NumberFormat))
 
 (defsc Car [this {:car/keys [id model] :as props}]
   {:query         [:car/id :car/model]
    :ident         :car/id
    :initial-state {:car/id    :param/id
                    :car/model :param/model}}
+  (js/console.log "Render car " id)
   (dom/div
     "Model " model))
 
@@ -29,6 +29,7 @@
                    :person/cars [{:id 40 :model "Leaf"}
                                  {:id 41 :model "Escort"}
                                  {:id 42 :model "Sienna"}]}}
+  (js/console.log "Render person " id)
   (let [onClick (comp/get-state this :onClick)]
     (div :.ui.segment
       (div :.ui.form
@@ -36,13 +37,13 @@
           (label {:onClick onClick} "Name: ")
           name)
         (div :.field
-          (label "Amount: ")
-          (ui-number-format {:value "1100221.33"
-                             :thousandSeparator true
-                             :prefix            "$"}))
-        (div :.field
           (label "Age: ")
           age)
+        (dom/button :.ui.button {:onClick (fn []
+                                            (comp/transact! this
+                                              `[(make-older ~{:person/id id})]
+                                              {:refresh [:person-list/people]}))}
+          "Make Older")
         (h3 {} "Cars")
         (ul {}
           (map ui-car cars))))))
@@ -52,23 +53,40 @@
 (defsc PersonList [this {:person-list/keys [people]}]
   {:query         [{:person-list/people (comp/get-query Person)}]
    :ident         (fn [] [:component/id ::person-list])
-   :initial-state {:person-list/people [{:id 99 :name "Joe"}]}}
-  (dom/ul
-    (map ui-person people)))
+   :initial-state {:person-list/people [{:id 1 :name "Bob"}
+                                        {:id 2 :name "Sally"}]}}
+  (js/console.log "Render list")
+  (let [cnt (reduce
+              (fn [c {:person/keys [age]}]
+                (if (> age 30)
+                  (inc c)
+                  c))
+              0
+              people)]
+    (div :.ui.segment
+      (h3 :.ui.header "People")
+      (div "Over 30: " cnt)
+      (dom/ul
+        (map ui-person people)))))
 
 (def ui-person-list (comp/factory PersonList))
 
-(defsc Sample [this {:root/keys [list]}]
+(defsc Root [this {:root/keys [list]}]
   {:query         [{:root/list (comp/get-query PersonList)}]
    :initial-state {:root/list {}}}
+  (js/console.log "Render root")
   (dom/div
     (dom/h3 "Application")
     (ui-person-list list)))
 
 (defonce APP (app/fulcro-app))
 
+(defmutation make-older [{:person/keys [id]}]
+  (action [{:keys [state]}]
+    (swap! state update-in [:person/id id :person/age] inc)))
+
 (defn ^:export init []
-  (app/mount! APP Sample "app"))
+  (app/mount! APP Root "app"))
 
 (defn get-components-that-query-for-a-prop
   [prop]
@@ -80,23 +98,24 @@
     (comp/prop->classes APP prop)))
 
 (comment
-  (comp/component-options Person)
 
-  (comp/transact! APP [(make-older {:person/id 1})])
+
+  (get-components-that-query-for-a-prop :person/age)
+
+  (def before (app/current-state APP))
+  (def after (app/current-state APP))
+
+  before
+  after
 
   (map
     comp/get-ident
-   (get-components-that-query-for-a-prop :person/name))
+    (get-components-that-query-for-a-prop :person/name))
 
   (let [state           (app/current-state APP)
         component-query (comp/get-query Person)
-        component-ident [:person/id 99]
+        component-ident [:person/id 1]
         starting-entity (get-in state component-ident)]
-    (fdn/db->tree component-query starting-entity state)
-    )
-
-  (merge/merge-component! APP Person {:person/id 1 :person/age 20})
-
-  (comp/get-initial-state Sample)
+    (fdn/db->tree component-query starting-entity state))
 
   )
