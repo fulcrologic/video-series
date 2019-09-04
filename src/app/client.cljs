7c3d197a8df9bacc4d8fcf50d2f16bd7ecb98ab9
Tony Kay <tony.kay@gmail.com>
2019-09-04 13:13:09 -0700

    dom + react

--- src/app/client.cljs
+++ src/app/client.cljs
@@ -1,11 +1,15 @@
 (ns app.client
   (:require
+    ["react-number-format" :as NumberFormat]
     [com.fulcrologic.fulcro.application :as app]
     [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
-    [com.fulcrologic.fulcro.dom :as dom]
+    [com.fulcrologic.fulcro.dom :as dom :refer [div ul li h3 label]]
     [com.fulcrologic.fulcro.algorithms.merge :as merge]
+    [com.fulcrologic.fulcro.algorithms.react-interop :as interop]
     [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
     [com.fulcrologic.fulcro.algorithms.data-targeting :as targeting]))
+
+(def ui-number-format (interop/react-factory NumberFormat))
 
 (defsc Car [this {:car/keys [id model] :as props}]
   {:query         [:car/id :car/model]
@@ -26,21 +30,43 @@
                    :person/cars [{:id 40 :model "Leaf"}
                                  {:id 41 :model "Escort"}
                                  {:id 42 :model "Sienna"}]}}
-  (dom/div
-    (dom/div "Name: " name)
-    (dom/div "Age: " age)
-    (dom/h3 "Cars")
-    (dom/ul
-      (map ui-car cars))))
+  (let [onClick (comp/get-state this :onClick)]
+    (div :.ui.segment
+      (div :.ui.form
+        (div :.field
+          (label {:onClick onClick} "Name: ")
+          name)
+        (div :.field
+          (label "Amount: ")
+          (ui-number-format {:value "1100221.33"
+                             :thousandSeparator true
+                             :prefix            "$"}))
+        (div :.field
+          (label "Age: ")
+          age)
+        (h3 {} "Cars")
+        (ul {}
+          (map ui-car cars))))))
 
 (def ui-person (comp/factory Person {:keyfn :person/id}))
 
-(defsc Sample [this {:root/keys [person]}]
-  {:query         [{:root/person (comp/get-query Person)}]
-   :initial-state {:root/person {:id 1 :name "Bob"}}}
+(defsc PersonList [this {:person-list/keys [people] :as props}]
+  {:query         [{:person-list/people (comp/get-query Person)}]
+   :ident         (fn [_ _] [:component/id ::person-list])
+   :initial-state {:person-list/people [{:id 1 :name "Bob"}
+                                        {:id 2 :name "Sally"}]}}
   (dom/div
-    (when person
-      (ui-person person))))
+    (h3 "People")
+    (map ui-person people)))
+
+(def ui-person-list (comp/factory PersonList))
+
+(defsc Sample [this {:root/keys [people]}]
+  {:query         [{:root/people (comp/get-query PersonList)}]
+   :initial-state {:root/people {}}}
+  (div
+    (when people
+      (ui-person-list people))))
 
 (defonce APP (app/fulcro-app))
 
@@ -48,6 +74,7 @@
   (app/mount! APP Sample "app"))
 
 (comment
+  (comp/component-options Person)
 
   (comp/transact! APP [(make-older {:person/id 1})])
 
